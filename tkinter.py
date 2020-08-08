70bf64e3c5992f4e570212ac1bfdc55c83ff1b93
AKgroup2000 <65353307+AKgroup2000@users.noreply.github.com>
2020-08-09 01:48:00 +0530

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -88,8 +88,8 @@ class PageThree(tk.Frame):
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-        f = Figure(figsize=(5,5), dpi=100)
-        a = f.add_subplot(111)
+        fig = Figure(figsize=(5,5), dpi=100)
+        a = fig.add_subplot(111)
         a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])        
         canvas = FigureCanvasTkAgg(f, self)
         canvas.show()
